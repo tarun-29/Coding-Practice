00247e5052450db7e3a0be68fd417db1c6183fb3
tarun-29 <coe17b031@iiitdm.ac.in>
2020-10-18 00:39:54 +0530

    20.cpp

--- HackerEarth/DataStructure/Array/20.cpp
+++ HackerEarth/DataStructure/Array/20.cpp
@@ -1,37 +1,40 @@
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
-#define mx 1000000007
+ 
+#define M 1000000007
+long long func(long long *sum,long long X,long long N)
+{
+    return (sum[X%N]+(((X/N)%M)*sum[N])%M)%M;
+}
 int main()
 {
-    int t;cin>>t;
-    while(t--)
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int T;
+    cin>>T;
+    while(T--)
     {
-        ll n;cin>>n;ll sum=0;
-        ll arr[n];
-        for(ll i=0;i<n;i++)
-        {cin>>arr[i];sum+=arr[i];} 
-        ll q;cin>>q;
-        ll left[q];ll right[q];
-        for(ll i=0;i<q;i++)
-        {cin>>left[i];left[i]--;}
-        for(ll i=0;i<q;i++)
-        {cin>>right[i];right[i]--;}
-        for(ll i=0;i<q;i++)
-        {
-            ll ans=0;
-            ll len=right[i]-left[i]+1;
-            ans=(ans+(((len/n)%mx)*sum)%mx)%mx;
-            ll le=left[i]%n;
-            ll ri=right[i]%n;
-            while(le!=ri)
-            {
-                ans=(ans+(arr[le]))%mx;
-                le=(le+1)%n;
-            }
-            cout<<(ans+arr[ri])%mx<<" ";
-        }
+        int N;
+        cin>>N;
+        long long arr[N+1];
+        for(int i=1;i<=N;i++)
+            cin>>arr[i];
+        int Q;
+        cin>>Q;
+        long long L[Q],R[Q];
+        for(int i=0;i<Q;i++)
+            cin>>L[i];
+        for(int i=0;i<Q;i++)
+            cin>>R[i];
+        long long sum[N+1];
+        sum[0]=0;
+        for(int i=1;i<=N;i++)
+            sum[i]=(sum[i-1]+arr[i])%M;
+        for(int i=0;i<Q;i++)
+            cout<<(func(sum,R[i],N)-func(sum,L[i]-1,N)+M)%M<<" ";
         cout<<endl;
     }
-	
+    
 }
